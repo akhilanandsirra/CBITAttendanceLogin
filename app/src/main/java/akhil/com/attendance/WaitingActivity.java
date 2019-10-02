e95cdf299865624c1941ae3146d503f6d0e3a2fa
Akhil <akhilanand9999@gmail.com>
2019-10-02 20:14:24 +0530

    jsoup code fixed

--- app/src/main/java/akhil/com/attendance/WaitingActivity.java
+++ app/src/main/java/akhil/com/attendance/WaitingActivity.java
@@ -1,5 +1,6 @@
 package akhil.com.attendance;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -7,6 +8,7 @@ import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Toast;
 
+import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -17,7 +19,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatTextView;
 
 public class WaitingActivity extends AppCompatActivity {
-    String at;
+    String at,at2;
     AppCompatTextView percentage;
     private Content Task;
     @Override
@@ -41,11 +43,45 @@ public class WaitingActivity extends AppCompatActivity {
         protected Void doInBackground(Void... voids) {
             try {
                 //Connect to the website
-                Document document = Jsoup.connect("https://erp.cbit.org.in/?__LASTFOCUS=&__EVENTTARGET=&__EVENTARGUMENT=&__VIEWSTATE=%2FwEPDwUKMTExMDk3MjkwOA9kFgICAQ9kFhQCBQ8PFgIeB1Zpc2libGVoZGQCCQ8PFgIfAGdkZAILDw8WBB4ISW1hZ2VVcmwFRH5cRVJQXEFkbWluXFN0dWRlbnRQaG90b3MuYXNweD9TdHVkSWQ9NzUwMiZDb2xDb2RlPTAwMDEmR3JwQ29kZT1DQklUHwBnZGQCDw8PFgYeBFRleHQFDDE2MDExNzczNzA4OR4HRW5hYmxlZGgfAGhkZAIRDw8WAh8AZ2RkAhMPDxYCHwJlZGQCFQ8PFgIfAGhkZAIXDw8WAh8AZ2RkAhsPDxYCHwBnZGQCHQ8PFgIfAGdkZBgBBR5fX0NvbnRyb2xzUmVxdWlyZVBvc3RCYWNrS2V5X18WAgUHSW1nQmFjawUMSW1nVXNlclBob3RvK%2BH5kGAx0PSv8AMiMnYMoI4uyXiMiYM6TasaCds24ME%3D&__VIEWSTATEGENERATOR=C2EE9ABB&__EVENTVALIDATION=%2FwEdAAm3b5ZXqJt8X%2FJx%2BD4jDeHOBjpuGLkudYNkCAonVyADt%2B5PVNfdHmla7NuBu7%2FwrMMyrNXnRHAQ0i9tgVPzigLD0vlayIibJlZm2HEdhP9alnY2%2BMc6SrnAqio3oCKbxYY85pbWlDO2hADfoPXD%2F5tdKHfjJrmhuOJtNRn2ZOc35Gp8QtNk8V97ULE6OJFh19NI2AIBKsDmJMvEpIUF8lCQZyBbrZ31FlP1tUsnDSExTg%3D%3D&txtPassword=8055&btnSubmit=Submit").get();
+
+                String url="https://erp.cbit.org.in/?__LASTFOCUS=&__EVENTTARGET=&__EVENTARGUMENT=&__VIEWSTATE=%2FwEPDwUKMTExMDk3MjkwOA9kFgICAQ9kFgICBQ8PFgIeB1Zpc2libGVoZGRkApSadXV8hBd7qi9M9MQf24gQFo1JDdpv3rqIRSVoR5Y%3D&__VIEWSTATEGENERATOR=C2EE9ABB&__EVENTVALIDATION=%2FwEdAAUo8HF9hHYWKGGF3Et0JGNxBjpuGLkudYNkCAonVyADt%2B5PVNfdHmla7NuBu7%2FwrMNjemWCTRgEB59HPczIGVNwgWOkgugWB5Cq9dYD7toQNEwZfb2PCk9YCZQ7UhXsjSWufILYgZp8zPh7f7XDtu2a&txtUserName=160117737089&btnNext=Next";
+                String url2="https://erp.cbit.org.in/";
+                Connection.Response loginForm = Jsoup.connect(url)
+                        .method(Connection.Method.GET)
+                        .userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:69.0) Gecko/20100101 Firefox/69.0")
+                        .execute();
+
+                Document doc = loginForm.parse();
+                Element e = doc.select("input[id=__VIEWSTATE]").first();
+                String viewState = e.attr("value");
+                e = doc.select("input[id=__EVENTVALIDATION]").first();
+                String eventValidation = e.attr("value");
+                e = doc.select("input[id=__LASTFOCUS]").first();
+                String lastFocus = e.attr("value");
+                e = doc.select("input[id=__EVENTTARGET]").first();
+                String eventTarget = e.attr("value");
+                e = doc.select("input[id=__VIEWSTATEGENERATOR]").first();
+                String viewstateGenerator = e.attr("value");
+                e = doc.select("input[id=__EVENTARGUMENT]").first();
+                String eventArgument = e.attr("value");
+
+                Document document = Jsoup.connect(url2)
+                        .data("cookieexists", "false")
+                        .data("txtPassword","8055")
+                        .data("btnSubmit","Submit")
+                        .data("__LASTFOCUS",lastFocus)
+                        .data("__EVENTTARGET",eventTarget)
+                        .data("__EVENTARGUMENT",eventArgument)
+                        .data("__VIEWSTATEGENERATOR",viewstateGenerator)
+                        .data("__VIEWSTATE", viewState)
+                        .data("__EVENTVALIDATION", eventValidation)
+                        .cookies(loginForm.cookies())
+                        .userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:69.0) Gecko/20100101 Firefox/69.0")
+                        .get();
+
                 Element attendance = document.getElementById("ctl00_cpStud_lblTotalPercentage");
                 at=attendance.text();
                 at=at.replaceAll("[^\\.0123456789]","");
-
 
             } catch (IOException e) {
                 Toast.makeText(WaitingActivity.this, "No Internet Connection", Toast.LENGTH_LONG).show();
@@ -57,7 +93,7 @@ public class WaitingActivity extends AppCompatActivity {
         protected void onPostExecute(Void aVoid) {
             super.onPostExecute(aVoid);
             Toast.makeText(WaitingActivity.this, at, Toast.LENGTH_LONG).show();
-            float percent=Float.valueOf(at);
+            //float percent=Float.valueOf(at);
             openResultativity();
         }
     }
