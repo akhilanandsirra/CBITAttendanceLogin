abb83e284f120266f5c89eb70fbf5781e79726d0
Akhil <akhilanand9999@gmail.com>
2019-10-05 17:38:11 +0530

    Exceptions Added

--- app/src/main/java/akhil/com/attendance/WaitingActivity.java
+++ app/src/main/java/akhil/com/attendance/WaitingActivity.java
@@ -4,22 +4,26 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Trace;
+import android.util.Log;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Toast;
 
 import org.jsoup.Connection;
+import org.jsoup.HttpStatusException;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 
 import java.io.IOException;
+import java.net.SocketTimeoutException;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatTextView;
 
 public class WaitingActivity extends AppCompatActivity {
-    String at,at2;
+    String at;
     AppCompatTextView percentage;
     private Content Task;
     @Override
@@ -47,7 +51,6 @@ public class WaitingActivity extends AppCompatActivity {
 
                 String UserName=intent.getStringExtra("Username");
                 String Password=intent.getStringExtra("Password");
-
                 String url="https://erp.cbit.org.in/?__LASTFOCUS=&__EVENTTARGET=&__EVENTARGUMENT=&__VIEWSTATE=%2FwEPDwUKMTExMDk3MjkwOA9kFgICAQ9kFgICBQ8PFgIeB1Zpc2libGVoZGRkApSadXV8hBd7qi9M9MQf24gQFo1JDdpv3rqIRSVoR5Y%3D&__VIEWSTATEGENERATOR=C2EE9ABB&__EVENTVALIDATION=%2FwEdAAUo8HF9hHYWKGGF3Et0JGNxBjpuGLkudYNkCAonVyADt%2B5PVNfdHmla7NuBu7%2FwrMNjemWCTRgEB59HPczIGVNwgWOkgugWB5Cq9dYD7toQNEwZfb2PCk9YCZQ7UhXsjSWufILYgZp8zPh7f7XDtu2a&txtUserName="+UserName+"&btnNext=Next";
                 String url2="https://erp.cbit.org.in/";
                 Connection.Response loginForm = Jsoup.connect(url)
@@ -55,39 +58,73 @@ public class WaitingActivity extends AppCompatActivity {
                         .userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:69.0) Gecko/20100101 Firefox/69.0")
                         .execute();
 
-                Document doc = loginForm.parse();
-                Element e = doc.select("input[id=__VIEWSTATE]").first();
-                String viewState = e.attr("value");
-                e = doc.select("input[id=__EVENTVALIDATION]").first();
-                String eventValidation = e.attr("value");
-                e = doc.select("input[id=__LASTFOCUS]").first();
-                String lastFocus = e.attr("value");
-                e = doc.select("input[id=__EVENTTARGET]").first();
-                String eventTarget = e.attr("value");
-                e = doc.select("input[id=__VIEWSTATEGENERATOR]").first();
-                String viewstateGenerator = e.attr("value");
-                e = doc.select("input[id=__EVENTARGUMENT]").first();
-                String eventArgument = e.attr("value");
+                    Document doc = loginForm.parse();
+                    Element e = doc.select("input[id=__VIEWSTATE]").first();
+                    String viewState = e.attr("value");
+                    e = doc.select("input[id=__EVENTVALIDATION]").first();
+                    String eventValidation = e.attr("value");
+                    e = doc.select("input[id=__LASTFOCUS]").first();
+                    String lastFocus = e.attr("value");
+                    e = doc.select("input[id=__EVENTTARGET]").first();
+                    String eventTarget = e.attr("value");
+                    e = doc.select("input[id=__VIEWSTATEGENERATOR]").first();
+                    String viewstateGenerator = e.attr("value");
+                    e = doc.select("input[id=__EVENTARGUMENT]").first();
+                    String eventArgument = e.attr("value");
 
-                Document document = Jsoup.connect(url2)
-                        .data("cookieexists", "false")
-                        .data("txtPassword",Password)
-                        .data("btnSubmit","Submit")
-                        .data("__LASTFOCUS",lastFocus)
-                        .data("__EVENTTARGET",eventTarget)
-                        .data("__EVENTARGUMENT",eventArgument)
-                        .data("__VIEWSTATEGENERATOR",viewstateGenerator)
-                        .data("__VIEWSTATE", viewState)
-                        .data("__EVENTVALIDATION", eventValidation)
-                        .cookies(loginForm.cookies())
-                        .userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:69.0) Gecko/20100101 Firefox/69.0")
-                        .get();
+                    Document document = Jsoup.connect(url2)
+                            .data("cookieexists", "false")
+                            .data("txtPassword",Password)
+                            .data("btnSubmit","Submit")
+                            .data("__LASTFOCUS",lastFocus)
+                            .data("__EVENTTARGET",eventTarget)
+                            .data("__EVENTARGUMENT",eventArgument)
+                            .data("__VIEWSTATEGENERATOR",viewstateGenerator)
+                            .data("__VIEWSTATE", viewState)
+                            .data("__EVENTVALIDATION", eventValidation)
+                            .cookies(loginForm.cookies())
+                            .userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:69.0) Gecko/20100101 Firefox/69.0")
+                            .get();
 
-                Element attendance = document.getElementById("ctl00_cpStud_lblTotalPercentage");
-                at=attendance.html();
-                at=at.replaceAll("[^\\.0123456789]","");
+                    Element attendance = document.getElementById("ctl00_cpStud_lblTotalPercentage");
+                    at=attendance.html();
+                    at=at.replaceAll("[^\\.0123456789]","");
 
-            } catch (IOException e) {
+            }
+            catch(NullPointerException e){
+
+                runOnUiThread(new Runnable(){
+                    public void run() {
+                        Toast.makeText(getApplicationContext(), "Password is Incorrect",Toast.LENGTH_LONG).show();
+                    }
+                });
+                Task.cancel(true);
+                finish();
+            }
+
+            catch(HttpStatusException e){
+
+                runOnUiThread(new Runnable(){
+                    public void run() {
+                        Toast.makeText(getApplicationContext(), "Username is Incorrect",Toast.LENGTH_LONG).show();
+                    }
+                });
+                Task.cancel(true);
+                finish();
+            }
+
+            catch(SocketTimeoutException e){
+
+                runOnUiThread(new Runnable(){
+                    public void run() {
+                        Toast.makeText(getApplicationContext(), "Session Timeout",Toast.LENGTH_LONG).show();
+                    }
+                });
+                Task.cancel(true);
+                finish();
+            }
+
+            catch (IOException e) {
                 Toast.makeText(WaitingActivity.this, "No Internet Connection", Toast.LENGTH_LONG).show();
             }
             return null;
@@ -107,6 +144,7 @@ public class WaitingActivity extends AppCompatActivity {
         intent.putExtra("percentage",at);
         startActivity(intent);
     }
+
 
     @Override
     public void onBackPressed() {
