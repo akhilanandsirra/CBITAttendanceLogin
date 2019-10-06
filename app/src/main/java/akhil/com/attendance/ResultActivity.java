cec8526b406266eb8893d49603fd8aec6420cf7b
Akhil <akhilanand9999@gmail.com>
2019-10-06 18:11:20 +0530

    New Result View Added

--- app/src/main/java/akhil/com/attendance/ResultActivity.java
+++ app/src/main/java/akhil/com/attendance/ResultActivity.java
@@ -12,12 +12,16 @@ import com.john.waveview.WaveView;
 
 public class ResultActivity extends AppCompatActivity {
     WaveView waveView;
-    AppCompatTextView Result;
+    AppCompatTextView Result,classAttended,classesTotal;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
         Result=(AppCompatTextView)findViewById(R.id.percent);
+        classAttended=(AppCompatTextView)findViewById(R.id.classes1);
+        classesTotal=(AppCompatTextView)findViewById(R.id.classes2);
+
         Window w = getWindow();
         w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
 
@@ -25,11 +29,17 @@ public class ResultActivity extends AppCompatActivity {
         String result=intent.getStringExtra("percentage");
         float percent=Float.valueOf(result);
 
+        String classesHeld=intent.getStringExtra("classes1");
+        String classesAttended=intent.getStringExtra("classes2");
+
         waveView=(WaveView)findViewById(R.id.wave_view);
         ProgressBarAnimation anim = new ProgressBarAnimation(waveView, 0, percent);
         anim.setDuration(1000);
         waveView.startAnimation(anim);
         Result.setText(String.format(getString(R.string.percentage),percent));
+        classAttended.setText(String.format(getString(R.string.attendedString),classesAttended));
+        classesTotal.setText(String.format(getString(R.string.totalString),classesHeld));
+
     }
 
     @Override
